2c4626598d9223605f962854ade983490efb57e6
drupchen <hhdrupchen@gmail.com>
2018-10-23 22:46:25 +0200

    allow to include original in the output

--- wylieconvert/convert.py
+++ wylieconvert/convert.py
@@ -22,7 +22,7 @@ def unicode2wylie(text, dir='Lingua-BO-Wylie'):
     return out
 
 
-def batch_convert(mode='u2w'):
+def batch_convert(mode, include_orig):
     # setting the folders
     in_path = Path('input')
     out_path = Path('output')
@@ -34,22 +34,33 @@ def batch_convert(mode='u2w'):
 
     # conversion
     to_convert = list(in_path.glob('*.txt'))
-    arg = ['-u'] if mode == 'u2w' else []
+    args = []
+    if mode == 'u2w':
+        args.append('-u')
+    if include_orig:
+        args.append('-r')
+        args.append('\n\t')
 
     current = os.getcwd()
     os.chdir('Lingua-BO-Wylie/bin/')
     for f in to_convert:
         in_file = str(Path.resolve(Path.cwd() / '..' / '..' / f))
         out_file = str(Path.resolve(Path.cwd() / '..' / '..' / str(out_path / f.name)))
-        Popen(['perl', 'wylie.pl'] + arg + [in_file, out_file])
+        Popen(['perl', 'wylie.pl', '-r', '\n\t'] + args + [in_file, out_file])
     os.chdir(current)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('requires a single argument:\n"u2w" for unicode2wylie\n"w2u" for wylie2unicode')
+    if 2 > len(sys.argv) > 3:
+        print('requires the following:\n"u2w" for unicode2wylie\n"w2u" for wylie2unicode\n\noptional:'
+              '\n"true" to include the original string')
     mode = sys.argv[1]
+    include_orig = False
+
     if mode != 'u2w' and mode != 'w2u':
-        print('argument should be:\n"u2w" for unicode2wylie\n"w2u" for wylie2unicode')
+        print('argument should be:\n"u2w" for unicode2wylie\n"w2u" for wylie2unicode'
+              '\n"true" to include the original string')
     else:
-        batch_convert(mode)
+        if len(sys.argv) == 3 and sys.argv[2] == 'true':
+            include_orig = True
+        batch_convert(mode, include_orig=include_orig)
