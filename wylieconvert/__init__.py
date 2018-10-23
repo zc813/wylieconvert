25329b9b425ebe61cc50361df386df50bbae17ad
drupchen <hhdrupchen@gmail.com>
2018-10-23 21:46:25 +0200

    add batch_convert

---
+++ wylieconvert/__init__.py
@@ -0,0 +1,3 @@
+from .convert import wylie2unicode, unicode2wylie
+
+__all__ = ['wylie2unicode', 'unicode2wylie']
--- wylieconvert/convert.py
+++ wylieconvert/convert.py
@@ -1,5 +1,7 @@
 from subprocess import Popen, PIPE
 import os
+import sys
+from pathlib import Path
 
 
 def wylie2unicode(text, dir='Lingua-BO-Wylie'):
@@ -20,9 +22,37 @@ def unicode2wylie(text, dir='Lingua-BO-Wylie'):
     return out
 
 
+def batch_convert(mode='u2w'):
+    # setting the folders
+    in_path = Path('input')
+    out_path = Path('output')
+    in_path.mkdir(exist_ok=True)
+    out_path.mkdir(exist_ok=True)
+
+    if not list(in_path.glob('*.txt')):
+        exit(print('There are no .txt files to convert'))
+
+    # conversion
+    to_convert = in_path.glob('*.txt')
+    if mode == 'u2w':
+        for f in to_convert:
+            content = f.read_text(encoding='utf-8-sig')
+            converted = unicode2wylie(content)
+            new = out_path / f.name
+            new.write_text(converted)
+    elif mode == 'w2u':
+        for f in to_convert:
+            content = f.read_text(encoding='utf-8-sig')
+            converted = wylie2unicode(content)
+            new = out_path / f.name
+            new.write_text(converted)
+
+
 if __name__ == '__main__':
-    wylie = 'bkra shis bde legs/'
-    unicode = wylie2unicode(wylie)
-    print(unicode)
-    re_wylie = unicode2wylie(unicode)
-    assert wylie == re_wylie
+    if len(sys.argv) != 2:
+        print('requires a single argument:\n"u2w" for unicode2wylie\n"w2u" for wylie2unicode')
+    mode = sys.argv[1]
+    if mode is not 'u2w' and mode is not 'w2u':
+        print('argument should be:\n"u2w" for unicode2wylie\n"w2u" for wylie2unicode')
+    else:
+        batch_convert(mode)
